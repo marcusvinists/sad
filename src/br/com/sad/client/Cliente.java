e9a1268b13403327d3b88646aee96b4cfa098b8f
vini <vini@shinrorobot2>
2016-10-28 21:29:28 -0200

    espaçamento

--- src/br/com/sad/client/Cliente.java
+++ src/br/com/sad/client/Cliente.java
@@ -18,14 +18,14 @@ public class Cliente {
 
     private static String nomeDoArquivo;
     private static String txt;
-    
+
     public static void main(String args[]) {
         try {
             System.out.println("Bem vindo ao SAD - Sistema de Arquivos Distrbuidos");
             Scanner scan = new Scanner(System.in);
             int portClient = 2011;
             Operacoes cs = (Operacoes) Naming.lookup("rmi://localhost:" + portClient + "/cs");
-            
+
             while (true) {
                 System.out.println("********************************");
                 System.out.println("Selecione a operaçao desejada:");
@@ -46,15 +46,15 @@ public class Cliente {
                         List lista = cs.listarArquivos();
                         System.out.println(" ");
                         System.out.println("********************************");
-                        if(lista.size()>0){
-                           System.out.println("Lista de arquivos: ");
-                        }else{
+                        if (lista.size() > 0) {
+                            System.out.println("Lista de arquivos: ");
+                        } else {
                             System.out.println("Nenhum arquivo a ser exibido!");
                         }
-                        int i =1;
-                        for(Object str : lista){
-                            System.out.println(i +"-"+ str.toString());
-                            i+=1;
+                        int i = 1;
+                        for (Object str : lista) {
+                            System.out.println(i + "-" + str.toString());
+                            i += 1;
                         }
                         System.out.println("********************************");
                         System.out.println("");
@@ -69,10 +69,10 @@ public class Cliente {
                     case 4:
                         String conteudo = cs.lerArquivo(nomeDoArquivo);
                         System.out.println("********************************");
-                        if(conteudo!=null){
-                         System.out.println("Conteúdo do arquivo: ");
+                        if (conteudo != null) {
+                            System.out.println("Conteúdo do arquivo: ");
                             System.out.println(conteudo);
-                        }else{
+                        } else {
                             System.out.println("Arquivo não encontrado");
                         }
                         System.out.println("********************************");
@@ -81,13 +81,12 @@ public class Cliente {
                         System.exit(0);
                         break;
                 }
-                
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
-    
+
     public static void digitarNomeOuCont(int servico) {
         Scanner scan = new Scanner(System.in);
         if (servico == 1 || servico == 4 || servico == 3) {
@@ -99,5 +98,5 @@ public class Cliente {
             txt = scan.nextLine();
         }
     }
-    
+
 }
